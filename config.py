765b34a72e96c969d2a165c30a5e543799e47f80
Cherry5990 <maelyss.fronton@etu.univ-orleans.fr>
2025-09-02 17:04:39 +0200

    Ajout pages about et contact accessible uniquement par URL mais pas par lien depuis index

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 #>>>import random, string, os
 #>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
 SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "Voici mon contact : pommedeterre@exemple.fr"
