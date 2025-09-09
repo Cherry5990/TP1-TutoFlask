7ca1525e60b1e6532bfdb256d307964082f7bb34
Cherry5990 <maelyss.fronton@etu.univ-orleans.fr>
2025-09-09 17:00:28 +0200

    Fin de séance : données de data.yml chargées dans monApp.db

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 #>>>import random, string, os
 #>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
+
+import os
+
 SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
 ABOUT = "Bienvenue sur la page à propos de Flask !"
 CONTACT = "Voici mon contact : pommedeterre@exemple.fr"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
