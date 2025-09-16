043ae7bdd6949b8f7a642da5b9e26ab21cd96eaf
Cherry5990 <maelyss.fronton@etu.univ-orleans.fr>
2025-09-16 16:33:39 +0200

    Pages transformées en templates, ajout de style.css

--- monApp/views.py
+++ monApp/views.py
@@ -1,16 +1,18 @@
 from .app import app
+from flask import render_template
 
 @app.route('/')
+@app.route('/index/') # toujours mettre "/index/" au lieu de "/index" car sinon si on tape "/index/" on n'y accède pas
 def index():
-    return "Hello world !"
+    return render_template("index.html", title ="R3.01 Dev Web avec Flask", name="WTH ??!!")
 
 @app.route('/about/')
 def about():
-    return app.config["ABOUT"]
+    return render_template("about.html", title ="R3.01 Dev Web avec Flask - about")
 
 @app.route('/contact/')
 def contact():
-    return app.config["CONTACT"]
+    return render_template("contact.html", title ="R3.01 Dev Web avec Flask - contact")
 
 if __name__== "__main__":
     app.run()
