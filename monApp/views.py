14546821f66a34e4e911275d386eec0cc0fb3b78
Cherry5990 <maelyss.fronton@etu.univ-orleans.fr>
2025-09-16 16:56:04 +0200

    Ajout de base.html et factorisation de index.html, about.html et contact.html

--- monApp/views.py
+++ monApp/views.py
@@ -1,10 +1,15 @@
 from .app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index/') # toujours mettre "/index/" au lieu de "/index" car sinon si on tape "/index/" on n'y accède pas
 def index():
-    return render_template("index.html", title ="R3.01 Dev Web avec Flask", name="WTH ??!!")
+    # si pas de paramètres
+    if len(request.args)==0:
+        return render_template("index.html", title="R3.01 Dev Web avec Flask", name="Erika")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html", title="R3.01 Dev Web avec Flask", name = param_name)
 
 @app.route('/about/')
 def about():
